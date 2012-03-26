240ec84917e740c37d816adec3bf47d705111c5f
Pier Luigi Balducci <your_email@youremail.com>
2012-03-26 16:52:03 +0200

    primo aggiornamento file1

--- 2012-03-26/file1.js
+++ 2012-03-26/file1.js
@@ -8,3 +8,26 @@ vad dom2 = DOMAIN([[1,3][1,4]])([2,3])
 vad dom3 = DOMAIN([[1,3][1,4][1,2]])([2,3,1])
 
 //li disegno con DRAW(dom) e coloro con COLOR([r,g,b])(dom)
+
+
+
+//mapping
+var domain = DOMAIN([[0,1]])([10]);
+
+var mapping = function (p) {
+  var u = p[0];
+
+  return [u, 1];
+};
+
+var mapped = MAP(mapping)(domain);
+
+
+
+//bisettrice con mappatura
+
+var bisettrice = function (p) {
+  var u = p[0];
+
+  return [u, u];
+};
