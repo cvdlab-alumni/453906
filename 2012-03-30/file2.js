dc3c7db623f04ad129ed8271e2d74dab396b6537
Pier Luigi Balducci <pierluigi@etnorock.com>
2012-03-30 20:39:46 +0200

    aggiunto cubo

--- 2012-03-30/file2.js
+++ 2012-03-30/file2.js
@@ -2,9 +2,6 @@
 var quad = SIMPLICIALCOMPLEX([[0,0],[1,0],[1,1],[0,1]])([[0,1,3],[1,2,3]]);
 DRAW(quad);
 
-var quad10 = REPLICA(10)([quad,T([1])(2)]); // non funziona
-DRAW(quad10);
-
 var quad10 = p.simplexGrid([REPLICA(10)([1,-1]),[1]])
 DRAW(quad10);
 
@@ -16,3 +13,19 @@ DRAW(ex)
 
 var quad1000 = p.simplexGrid([REPLICA(10)([1,-1]),REPLICA(10)([1,-1]),REPLICA(10)([1,-1])])
 DRAW(quad1000)
+
+var triangoloEquilatero = SIMPLICIALCOMPLEX([[0.5,-1*Math.sqrt(3)/4],[-0.5,-1*Math.sqrt(3)/4],[0,Math.sqrt(3)/4]])([[0,2,1]]) //non è centrato nell'origine
+DRAW(triangoloEquilatero)
+
+var tetraedroCamafro = SIMPLICIALCOMPLEX([[0,0,0],[0,1,0],[1,0,0],[0,0,1]])([[0,1,2,3]])
+DRAW(tetraedroCamafro)
+
+
+
+//scheletro del cubo con dentro il tetraedro costruito sulle sue diagonali
+
+var tetraedroNelCubo = SIMPLICIALCOMPLEX([[0,0,0],[1,0,0],[1,1,0],[0,1,0],[0,0,1],[1,0,1],[1,1,1],[0,1,1]])([[1,3,4,6]])
+DRAW(tetraedroNelCubo)
+
+var cubo = SIMPLICIALCOMPLEX([[0,0,0],[1,0,0],[1,1,0],[0,1,0],[0,0,1],[1,0,1],[1,1,1],[0,1,1]])([[1,2,3,6],[1,0,3,4],[4,5,6,1],[4,7,6,3],[1,3,4,6]])
+DRAW(SKELETON(1)(cubo))
